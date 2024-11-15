5c2a6fe28ffd6dd51f9ff1c1d1663b498e94d490
Blasty25 <sohamshah210@gmail.com>
2024-11-14 22:05:07 -0500

    Adding Swerve base code with a few basic tweaks

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+public class Constants {
+
+    public class DriveConstants{
+        public static final int frontLeftDrive = 1;
+        public static final int frontLeftTurn = 2;
+        public static final int frontRightDrive = 3;
+        public static final int frontRightTurn = 4;
+        public static final int backLeftDrive = 5;
+        public static final int backLeftTurn = 6;
+        public static final int backRightDrive = 7;
+        public static final int backRightTurn = 8;
+
+        public static final int driveMotorChannel = 1;
+        public static final int turningMotorChannel = 2;
+        
+        public static final int frontLeftEncoder = 2;
+        public static final int frontRightEncoder = 3;
+        public static final int backLeftEncoder = 1;
+        public static final int backRightEncoder = 0;
+    }
+    
+}
---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,306 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import frc.robot.Constants;
+
+/** Represents a swerve drive style drivetrain. */
+public class Drivetrain {
+  public static final double kMaxSpeed = 3.0; // 3 meters per second
+  public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
+
+  private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+  private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+  private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+  private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+
+  SwerveModule swerveModule;
+
+  SwerveModule frontLeft = new SwerveModule(Constants.DriveConstants.frontLeftEncoder, Constants.DriveConstants.frontRightEncoder, Constants.DriveConstants.backLeftEncoder, Constants.DriveConstants.backRightEncoder);
+  SwerveModule frontRight = new SwerveModule(Constants.DriveConstants.frontLeftEncoder, Constants.DriveConstants.frontRightEncoder, Constants.DriveConstants.backLeftEncoder, Constants.DriveConstants.backRightEncoder);
+  SwerveModule backLeft = new SwerveModule(Constants.DriveConstants.frontLeftEncoder, Constants.DriveConstants.frontRightEncoder, Constants.DriveConstants.backLeftEncoder, Constants.DriveConstants.backRightEncoder);
+  SwerveModule backRight = new SwerveModule(Constants.DriveConstants.frontLeftEncoder, Constants.DriveConstants.frontRightEncoder, Constants.DriveConstants.backLeftEncoder, Constants.DriveConstants.backRightEncoder);
+
+  private final AnalogGyro m_gyro = new AnalogGyro(10);
+
+  private final SwerveDriveKinematics m_kinematics =
+      new SwerveDriveKinematics(
+          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+  private final SwerveDriveOdometry m_odometry =
+      new SwerveDriveOdometry(
+          m_kinematics,
+          m_gyro.getRotation2d(),
+          new SwerveModulePosition[] {
+            frontLeft.getPosition(),
+            frontRight.getPosition(),
+            backLeft.getPosition(),
+            backRight.getPosition()
+          });
+
+  public Drivetrain() {
+    m_gyro.reset();
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed Speed of the robot in the x direction (forward).
+   * @param ySpeed Speed of the robot in the y direction (sideways).
+   * @param rot Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+  public void drive(
+      double xSpeed, double ySpeed, double rot, boolean fieldRelative, double periodSeconds) {
+    var swerveModuleStates =
+        m_kinematics.toSwerveModuleStates(
+            ChassisSpeeds.discretize(
+                fieldRelative
+                    ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                        xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+                    : new ChassisSpeeds(xSpeed, ySpeed, rot),
+                periodSeconds));
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
+    frontLeft.setDesiredState(swerveModuleStates[0]);
+    frontRight.setDesiredState(swerveModuleStates[1]);
+    backLeft.setDesiredState(swerveModuleStates[2]);
+    backRight.setDesiredState(swerveModuleStates[3]);
+  }
+
+  /** Updates the field relative position of the robot. */
+  public void updateOdometry() {
+    m_odometry.update(
+        m_gyro.getRotation2d(),
+        new SwerveModulePosition[] {
+          frontLeft.getPosition(),
+          frontRight.getPosition(),
+          backLeft.getPosition(),
+          backRight.getPosition()
+        }); 
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Hi its Neil :)
